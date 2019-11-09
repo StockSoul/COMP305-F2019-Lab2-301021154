1dd5a4f78ff3d4e4e5631736fafca93f1a73f3eb
Andrew Trinidad <atrinid7my.centennialcollege.ca>
2019-11-09 14:29:59 -0500

    Added Scriptable Objects for Scene Settings

---
+++ Assets/_Scripts/Scene Settings/SceneSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Andrew Trinidad
+ * 3010201154
+ * Program Modifications: Created File to manage scenes
+*/
+
+[CreateAssetMenu(fileName = "SceneSettings", menuName = "Scene/Settings")]
+[System.Serializable]
+public class SceneSettings : ScriptableObject
+{
+    [Header("Scene Configuration")]
+    public SoundClip activeSoundClip;
+
+    [Header("ScoreBoard Labels")]
+    public bool scoreLabelEnbled;
+    public bool livesLabelEnabled;
+    public bool highScoreLabelEnabled;
+
+    [Header("Scene Labels")]
+    public bool startLabelActive;
+    public bool endLabelActive;
+
+    [Header("Scene Buttons")]
+    public bool startButtonActive;
+    public bool restartButtonActive;
+}
